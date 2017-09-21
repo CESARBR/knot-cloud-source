a02dc123841797c97880eeab80cd627df8622e59
Douglas Apolinario <doliveira.apolinario@gmail.com>
2017-09-21 10:43:40 -0300

    Configured the dockerfile

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo ""
-echo "##################################### Konker Cloud Integration #######################################"
+echo "##################################### Konker Cloud Integration ###################################"
 echo "##                                  Release date: 2017-09-20                                    ##"
 echo "##              Licence: Apache V2 (http://www.apache.org/licenses/LICENSE-2.0)                 ##"
 echo "##                           Need Support?: support@konkerlabs.com                              ##"
